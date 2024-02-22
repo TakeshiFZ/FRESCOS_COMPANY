448de050dfd50c00ef893469c4ebc970d643994e
TakeshiFZ <152714506+TakeshiFZ@users.noreply.github.com>
2024-02-22 18:44:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,5 @@ window.addEventListener("load", function (event) {
     alert("¡Todos los recursos terminaron de cargar!")
   });
 window.addEventListener("beforeunload", function (event) {
-    var mensaje = "¿Tas seguro de salir?"
     event.returnValue
   }); 
